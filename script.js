9ab2a34e5f5c67705472f0a0ec36b835edfd1c9f
Samuel Mariano <mariano.samuel@escola.pr.gov.br>
2026-09-10 18:57:48 -0300

    update menu-mobile

---
+++ script.js
@@ -0,0 +1,6 @@
+const botaoMenu = document.getElementById('menu-mobile');
+const nav = document.getElementById('nav');
+
+botaoMenu.addEventListener('click', () => {
+    nav.classList.toggle('ativo');
+});
